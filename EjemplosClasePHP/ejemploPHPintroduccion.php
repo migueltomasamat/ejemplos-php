28d4296d219805b790594e741cdfd736fc935f40
Miguel Ángel Tomás Amat <44135060+migueltomasamat@users.noreply.github.com>
2021-09-28 15:38:06 +0200

    Ejercicios introducción PHP
    Añadidos los enunciados de la introducción a PHP

---
+++ EjemplosClasePHP/ejemploPHPintroduccion.php
@@ -0,0 +1,7 @@
+    /*Declara una constante que se llame PI*/
+    //Declara una variable que vamos a llamar: numeroEntero
+    //En la siguiente instrucción (nueva línea en el script) asígnale el valor numérico: 1625
+    //Luego, en la siguiente instrucción (nueva línea) se deberá mostrar el valor de dicha variable en la página web con el siguiente texto: «El valor de &numeroEntero es …..» (en los puntos suspensivos debe aparecer el valor 1625)
+    //Muestra por pantalla el resultado de la suma de el valor que contiene la variable numeroEntero con la constante PI
+    //Crea una variable que sea una referencia a la variable numeroEntero y en la siguiente instrucción asigna el valor ‘un número’.
+    //Muestra el texto «El valor de &numeroEntero ahora es …..» 
