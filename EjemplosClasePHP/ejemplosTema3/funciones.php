4a3e5b18828bba0cf2e9c1a0095b33b6492c0283
Miguel Ángel Tomás Amat <miguel@cliMiguel.asir2.iespacomolla.es>
2021-11-10 20:37:49 +0100

    Clase 10-11-21

--- EjemplosClasePHP/ejemplosTema3/funciones.php
+++ EjemplosClasePHP/ejemplosTema3/funciones.php
@@ -60,3 +60,29 @@ function generarCierreHTML(){
         </body>
         </html>";
 }
+
+function comprobarUsuario($usuario,$pass){
+    $con = conectarBD();
+    $sql = "SELECT pass FROM usuario WHERE nombre = ?";
+
+    //Se prepara la sentencia 
+    $sentencia = mysqli_prepare($con,$sql);
+
+    //Se asocian los parámetros a la sentencia y se ejecuta la misma
+    mysqli_stmt_bind_param($sentencia,"s",$usuario);
+    mysqli_stmt_execute($sentencia);
+
+    //Acceso a los resultados de la ejcución de la consulta
+    $pass_cifrado;
+    mysqli_stmt_bind_result($sentencia,$pass_cifrado);
+
+    //Se mueve a la siguiente fila del conjunto de resultados, en este caso solamente uno.
+    mysqli_stmt_fetch($sentencia);
+
+    //Se comprueba que la contraseña sea correcta
+    if (password_verify($pass,$pass_cifrado)){
+        return true;
+    }else{
+        return false;
+    }
+}
